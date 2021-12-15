19af169cf37a3639f2ca3070d8636cf088fa34d6
abdservus <abdccassiano@hotmail.com>
2021-12-14 18:48:09 -0600

    Curso React: empezando en App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [linea1, setLinea1] = useState('Hola mundo');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <select>
+        <option value="fire">Casa en llamas</option>
+        <option value="history">Futuramas</option>
+        <option value="history">History channel</option>
+        <option value="matriz">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select><br />
+
+      <input type="text" placeholder="Linea 1" /><br />
+      <input type="text" placeholder="Linea 2" /><br />
+      <button>Exportar</button>
+
+      <div>
+        <span>{linea1}</span><br />
+        <span>Linea 2</span>
+        <img src="" />
+      </div>
+      
     </div>
   );
 }
