87b43ff81d5a66868f17d9d66aba6b9e2949d296
abdservus <abdccassiano@hotmail.com>
2021-12-14 19:04:02 -0600

    Curso React: Agregando las imagenes

--- src/App.js
+++ src/App.js
@@ -4,28 +4,42 @@ import './App.css';
 
 function App() {
 
-  const [linea1, setLinea1] = useState('Hola mundo');
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function (evento) {
+    setLinea1(evento.target.value)
+  }
+
+  const onChangeLinea2 = function (evento) {
+    setLinea2(evento.target.value)
+  }
+
+  const onChangeImagen = function (evento) {
+    setImagen(evento.target.value)
+  }
 
   return (
     <div className="App">
 
-      <select>
+      <select onChange={onChangeImagen}>
         <option value="fire">Casa en llamas</option>
-        <option value="history">Futuramas</option>
+        <option value="futurama">Futuramas</option>
         <option value="history">History channel</option>
-        <option value="matriz">Matrix</option>
+        <option value="matrix">Matrix</option>
         <option value="philosoraptor">Philosoraptor</option>
         <option value="smart">Smart Guy</option>
       </select><br />
 
-      <input type="text" placeholder="Linea 1" /><br />
-      <input type="text" placeholder="Linea 2" /><br />
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1" /><br />
+      <input onChange={onChangeLinea2} type="text" placeholder="Linea 2" /><br />
       <button>Exportar</button>
 
       <div>
         <span>{linea1}</span><br />
-        <span>Linea 2</span>
-        <img src="" />
+        <span>{linea2}</span>
+        <img src={"/img/" + imagen + ".jpg"} />
       </div>
       
     </div>
